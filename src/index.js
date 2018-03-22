087664fdd8607677d4e583b8c5af4de8a5e76459
yiliang <yiliang.lai@gmail.com>
2018-03-22 21:06:36 +0800

    feat: 增加zhHantEncode，將筆畫資訊編入字串中

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 export { default as zhHantCompare } from './zhHantCompare.js';
+export { default as zhHantEncode } from './zhHantEncode.js';
---
+++ src/zhHantEncode.js
@@ -0,0 +1,36 @@
+import unicodeToStrokeData from './unicode_to_stroke.data';
+
+const unicodeToStroke = JSON.parse(unicodeToStrokeData);
+
+const MAPPING = '123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+function zhHantEncode(s) {
+  if (s == null) return s;
+
+  const str = (typeof s === 'string') ? s : String(s);
+  var code = '';
+
+  var n = 0;
+  while(true) {
+    var p = str.codePointAt(n);
+    var k = unicodeToStroke[p.toString(32)];
+
+    var c = String.fromCodePoint(p);
+
+    if (k != null) {
+      code = code + MAPPING.charAt(k) + c;
+    } else {
+      code = code + '0' + c;
+    }
+
+    if (p > 0xffff) {
+      n += 2;
+    } else {
+      n += 1;
+    }
+    if (n >= str.length) break;
+  }
+  return code;
+}
+
+export default zhHantEncode;
